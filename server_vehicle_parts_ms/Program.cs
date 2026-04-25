556a587a415b2083bc90e04c8757c4e643b62bd7
1hexcode <np01cp4a230335@islingtoncollege.edu.np>
2026-04-25 22:02:38 +0545

    add DI for login, create & jwt

--- server_vehicle_parts_ms/Program.cs
+++ server_vehicle_parts_ms/Program.cs
@@ -1,12 +1,62 @@
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+using server_vehicle_parts_ms.Data;
+using server_vehicle_parts_ms.Data.Entities;
+using server_vehicle_parts_ms.Services.Implementation;
+using server_vehicle_parts_ms.Services.Interface;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
 builder.Services.AddSwaggerGen();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true,
+
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(key)
+        };
+    });
+
+builder.Services.Configure<Users>(
+    builder.Configuration.GetSection("Users")
+);
+
+builder.Services.AddDbContext<AppDbContext>(
+    (options) => { options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")); }
+);
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<TokenService>();
+builder.Services.AddScoped<LoginService>();
 
 var app = builder.Build();
 
@@ -20,6 +70,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
