efb65ef880935c17b2a8dcc233e4389d726c6e8d
1hexcode <np01cp4a230335@islingtoncollege.edu.np>
2026-04-26 10:07:37 +0545

    add VehicleParts entity to manage vehicle parts with timestamps

---
+++ server_vehicle_parts_ms/Data/Entities/VehicleParts.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace server_vehicle_parts_ms.Data.Entities;
+
+public class VehicleParts : IHasTimestamps
+{
+    [Key]
+    public Guid Id { get; set; }
+    public Guid CategoryId { get; set; }
+    public PartCategories Category { get; set; }
+    [Required]
+    [StringLength(150)]
+    public string Name { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string Sku { get; set; }
+    public string? Description { get; set; }
+    public decimal CostPrice { get; set; }
+    public decimal UnitPrice { get; set; }
+    public int StockQuantity { get; set; }
+    public int ReorderLevel { get; set; } = 10;
+    public bool isActive { get; set; } = true;
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset? UpdatedAt { get; set; }
+}
