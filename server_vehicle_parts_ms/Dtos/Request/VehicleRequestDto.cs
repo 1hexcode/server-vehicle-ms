3ac96465520570c1df929b9af98d1de58c4eaa63
1hexcode <np01cp4a230335@islingtoncollege.edu.np>
2026-04-26 10:08:57 +0545

    add VehicleRequestDto for vehicle management

---
+++ server_vehicle_parts_ms/Dtos/Request/VehicleRequestDto.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using server_vehicle_parts_ms.Data.Entities;
+
+namespace server_vehicle_parts_ms.Dtos.Request;
+
+public class VehicleRequestDto
+{
+    [Required]
+    [StringLength(50)]
+    public string VehicleNumber { get; set; }
+    [Required]
+    public VehicleType Type { get; set; }
+    public string? Make { get; set; }
+    public string? Model { get; set; }
+    public int? Year { get; set; }
+    public string? Color { get; set; }
+}
