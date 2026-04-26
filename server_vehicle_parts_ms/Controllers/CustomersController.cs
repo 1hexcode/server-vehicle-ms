906ae96b83ad71f8b8a604c77c44adac075b3663
1hexcode <np01cp4a230335@islingtoncollege.edu.np>
2026-04-26 10:04:12 +0545

    add CustomersController with customer registration endpoint

---
+++ server_vehicle_parts_ms/Controllers/CustomersController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using server_vehicle_parts_ms.Dtos.Request;
+using server_vehicle_parts_ms.Services.Interface;
+
+namespace server_vehicle_parts_ms.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize(Roles = "Admin,Staff")]
+public class CustomersController(IUserService userService) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> RegisterCustomer([FromBody] RegisterUserDto dto)
+    {
+        var response = await userService.CreateCustomerAsync(dto);
+        return Ok(response);
+    }
+}
