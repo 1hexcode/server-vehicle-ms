e3bdc7c7de3169b6860b7f5ea571d43b2f3acb4d
1hexcode <np01cp4a230335@islingtoncollege.edu.np>
2026-04-26 10:08:11 +0545

    add PartCategoryRequestDto for managing part category requests

---
+++ server_vehicle_parts_ms/Dtos/Request/PartCategoryRequestDto.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using server_vehicle_parts_ms.Data.Entities;
+
+namespace server_vehicle_parts_ms.Dtos.Request;
+
+public class PartCategoryRequestDto
+{
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; }
+    [Required]
+    public VehicleType VehicleType { get; set; }
+    public Guid? ParentId { get; set; }
+}
