0b744c64facc0a4559427233f11fadddcfd16569
1hexcode <np01cp4a230335@islingtoncollege.edu.np>
2026-04-26 10:07:42 +0545

    add Vehicles entity to manage vehicle details with timestamps

---
+++ server_vehicle_parts_ms/Data/Entities/Vehicles.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace server_vehicle_parts_ms.Data.Entities;
+
+public class Vehicles : IHasTimestamps
+{
+    [Key]
+    public Guid Id { get; set; }
+    public Guid CustomerId { get; set; }
+    public Users Customer { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string VehicleNumber { get; set; }
+    public VehicleType Type { get; set; }
+    [StringLength(50)]
+    public string? Make { get; set; }
+    [StringLength(50)]
+    public string? Model { get; set; }
+    public int? Year { get; set; }
+    [StringLength(30)]
+    public string? Color { get; set; }
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset? UpdatedAt { get; set; }
+}
